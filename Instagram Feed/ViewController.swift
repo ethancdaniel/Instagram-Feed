ee6f1ae33fc8edcdc8fd6fa69b15a467a559551f
Ethan Daniel <ethancdaniel@gmail.com>
2018-12-24 14:47:36 -0800

    Initial Commit

---
+++ Instagram Feed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram Feed
+//
+//  Created by Ethan Daniel on 12/24/18.
+//  Copyright © 2018 Ethan Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
